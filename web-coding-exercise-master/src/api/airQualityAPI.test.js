7c08e464f2063ee3470ca49fe2a874bf1aec4037
archerjessie <66236559+archerjessie@users.noreply.github.com>
2020-10-08 18:08:51 +1100

    refactored version created

---
+++ web-coding-exercise-master/src/api/airQualityAPI.test.js
@@ -0,0 +1,24 @@
+import { getFromUrl } from './airQualityAPI'
+import fetchMock from 'jest-fetch-mock';
+
+beforeAll(() => {
+    fetchMock.enableMocks();
+})
+
+beforeEach(() => {
+    fetch.resetMocks();
+});
+
+describe("getFromUrl", () => {
+    it("should fetch from the url and pass data to callback", () => {
+        fetch.mockResponseOnce(JSON.stringify({ data: "test data" }));
+        const url = "https://google.com";
+        const callback = jest.fn(data => {
+            expect(data).toEqual("test data")
+        });
+
+        getFromUrl(url, callback);
+
+        expect(fetch).toHaveBeenCalledWith(url);
+    })
+})
